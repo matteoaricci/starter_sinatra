38dbc22dc680ef95b7313a2a4173010fae559c5e
github username <laurenkyu@gmail.com>
2020-02-20 16:13:52 -0500

    pokemonS

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,5 +1,6 @@
 class PokemonController < Sinatra::Base
     set(:views, "app/views/pokemon")
+    set :method_override, true
 
     get '/pokemons' do
         erb :index
@@ -13,6 +14,24 @@ class PokemonController < Sinatra::Base
         @name = params[:name]
         @poke_type = params[:poke_type]
         pokemon = Pokemon.create(name: @name, poke_type: @poke_type)
-        redirect to '/pokemons'
+        redirect to "/pokemons/#{pokemon.id}"
     end
+
+    get '/pokemons/:id' do
+        @pokemon = Pokemon.all.find(params[:id])
+
+        erb :show
+    end
+
+    get '/pokemons/:id/edit' do
+        @pokemon = Pokemon.all.find(params[:id])
+        erb :edit
+    end
+
+    patch '/pokemons/:id' do
+        pokemon = Pokemon.all.find(params[:id])
+        pokemon.update(name: params[:name], poke_type: params[:poke_type])
+        redirect "/pokemons/#{pokemon.id}"
+    end
+
 end
